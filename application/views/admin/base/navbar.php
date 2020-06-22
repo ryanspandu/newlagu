a274d03122f8903c3398cbf68600bb93b5e2c901
ryanspandu <1910036@gmail.com>
2020-06-22 22:20:21 +0700

    navbar black edition

--- application/views/admin/base/navbar.php
+++ application/views/admin/base/navbar.php
@@ -1,24 +1,24 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-main border d-flex flex-column">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark navbar-main d-flex flex-column">
     <div class="container">   
         <div class="d-flex flex-row justify-content-between w-100 align-items-center">
             <div class="d-flex felx-row align-items-center">
                 <a class="navbar-brand font-weight-bold" href="#">Dashboard</a>
                 <div class="d-none d-sm-flex flex-row">
-                    <a href="#" class="mx-2 text-dark">Lagu baru</a>
-                    <a href="#" class="mx-2 text-dark">Kompetitor</a>
-                    <a href="#" class="mx-2 text-dark">Statistik</a>
+                    <a href="#" class="mx-2 text-light">Lagu baru</a>
+                    <a href="#" class="mx-2 text-light">Kompetitor</a>
+                    <a href="#" class="mx-2 text-light">Statistik</a>
                 </div>
             </div>
-            <div class="d-flex d-sm-none flex-row align-items-center pointer profile-nav-btn" onclick="collapseMob()">
+            <div class="d-flex d-sm-none flex-row align-items-center pointer profile-nav-btn text-primary" onclick="collapseMob()">
                 Ryan
-                <div class="bg-secondary rounded-circle ml-3 overflow-hidden" style="width: 40px; height: 40px;">
-                    <img src="" class="" width="40px"/>
+                <div class="bg-primary rounded-circle ml-3 overflow-hidden" style="width: 40px; height: 40px;">
+                    <img src="<?php echo asset_url(); ?>img/panda.png" class="" width="40px"/>
                 </div>
             </div>
-            <div class="d-none d-sm-flex flex-row align-items-center pointer profile-nav-btn" onclick="collapseBs()">
+            <div class="d-none d-sm-flex flex-row align-items-center pointer profile-nav-btn text-primary" onclick="collapseBs()">
                 Ryan
-                <div class="bg-secondary rounded-circle ml-3 overflow-hidden" style="width: 40px; height: 40px;">
-                    <img src="" class="" width="40px"/>
+                <div class="bg-primary rounded-circle ml-3 overflow-hidden" style="width: 40px; height: 40px;">
+                    <img src="<?php echo asset_url(); ?>img/panda.png" class="" width="40px"/>
                 </div>
             </div>
         </div>
@@ -26,15 +26,15 @@
     <div class="container navbar-popup-m">
         <div id="search-mobile" class="w-100 d-sm-none collapse my-2">
             <div class="container d-flex flex-column align-items-start mt-3">
-                <a href="#" class="text-dark">Profile</a>
+                <a href="#" class="text-light">Profile</a>
                 <hr class="w-100"/>
-                <a href="#" class="text-dark">Lagu baru</a>
+                <a href="#" class="text-light">Lagu baru</a>
                 <hr class="w-100"/>
-                <a href="#" class="text-dark">Kompetitor</a>
+                <a href="#" class="text-light">Kompetitor</a>
                 <hr class="w-100"/>
-                <a href="#" class="text-dark">Statistik</a>
+                <a href="#" class="text-light">Statistik</a>
                 <hr class="w-100"/>
-                <a href="#" class="text-dark">Logout</a>
+                <a href="#" class="text-light">Logout</a>
             </div>
         </div>
     </div>    
