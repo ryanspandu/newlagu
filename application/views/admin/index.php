1376c47e54e670d5eca90436eeddca3319e597ad
ryanspandu <1910036@gmail.com>
2020-06-25 23:36:22 +0700

    main container update

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -36,7 +36,7 @@
         <div class="col-12 col-sm-8 mt-4 mt-sm-0">
             <div id="content" class="row">
                 <!-- Lagu baru -->
-                <div class="col-12">    
+                <div class="col-12 section-1">    
                     <h3 class="mb-4">Lagu Terbaru</h3>
                     <div class="owl-carousel owl-theme">
                         <div class="item">
@@ -98,7 +98,7 @@
                     </div>
                 </div>
                 <!-- Section 2 -->
-                <div class="col-12">
+                <div class="col-12 section-2">
                     <div class="row">
                         <div class="col-12 col-sm-6">
                             <div class="card mr-2 border-0 bg-light" style="">
@@ -127,7 +127,10 @@
                     </div>
                 </div>
                 <!-- Section 3 -->
-                <div class="col-12 mt-3">
+                <div class="col-12 mt-3 section-3">
+                    <div class="bg-primary px-4 pt-2 d-flex align-items-center justify-content-center text-light pointer view-music" style="width: 95px;">
+                        <i class="material-icons vicon">highlight_off</i><p class="mb-0 ml-1 vtext">Close</p>
+                    </div>
                     <div class="bg-primary p-2">
                         <form class="bg-primary d-flex flex-row w-100">
                             <input class="mr-sm-2 w-100 i-search px-3 py-2" type="search" placeholder="Cari lagu..." aria-label="Search">
@@ -137,7 +140,7 @@
                     <div class="mt-3 song">
                         <?php $no = 0; ?>
                         <?php for($i=1; $i<=10; $i++){ ?>
-                        <div class="p-3 bg-light rounded-lg d-flex flex-row mt-3">
+                        <div class="p-3 bg-light rounded-lg d-flex flex-row mt-3 bgsong">
                             <div class="numsong rounded-circle shadow bg-warning d-flex justify-content-center align-items-center font-weight-bold">
                                 <?php echo ++$no; ?>
                             </div>
@@ -157,6 +160,24 @@
 </div>
 </body>
 <script>
+let clicked = 0;
+$('.view-music').click(function(){
+    if(clicked == 0){
+        $('.section-1').hide(500);
+        $('.section-2').hide(500);
+        $('.vicon').text('expand_more');
+        $('.vtext').text('Open');
+        $('.section-3').removeClass('mt-3');
+        clicked = 1;
+    }else{
+        $('.section-1').show(500);
+        $('.section-2').show(500);
+        $('.vicon').text('highlight_off');
+        $('.vtext').text('Close');
+        $('.section-3').addClass('mt-3');
+        clicked = 0;
+    }
+});
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
